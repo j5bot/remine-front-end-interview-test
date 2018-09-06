dd1914db1467675d48eb406048c8a0870e6c6966
Jonathan ‘J5’ Cook <jonathan.j5.cook@gmail.com>
2018-09-05 23:28:46 -0400

    Update state of InterviewTestContainer
    Add buildingTypes

--- front-end-interview-test-client/src/InterviewTestContainer.js
+++ front-end-interview-test-client/src/InterviewTestContainer.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import RemineTable from './components/Table/RemineTable/RemineTable';
+import API from './API';
 
 class InterviewTestContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.initializeState();
+    }
+
+    initializeState() {
+        this.state = {
+            buildingTypes: [],
+            locations: []
+        };
+    }
+
+    componentDidMount() {
+        this.initializeBuildingTypes();
+    }
+
+    initializeBuildingTypes() {
+        const buildingTypesPromise = API.getBuildingTypes();
+
+        buildingTypesPromise.then(
+            (resolved) => {
+                this.setState({buildingTypes: resolved.data});
+            }
+        );
+    }
+
     render() {
+        const state = this.state;
+
         return (
             <div className="interviewTestContainer">
                 <div className="filterContainer">
                     Your filters go here.
                 </div>
-                <RemineTable properties={[]} />
+                <RemineTable properties={state.locations} />
             </div>
         );
     }
